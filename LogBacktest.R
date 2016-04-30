32f35e1ff52c7aff1d7602425d426795ed376f37
Arpan Gupta <Arpan Gupta>
2016-04-30 00:10:50 -0400

    Fixed error with preturns and pdates not matching up

--- LogBacktest.R
+++ LogBacktest.R
@@ -10,6 +10,7 @@ returns <- c(0)
 preturns <- c() #unrealized and realized returns
 pdates <- c() #dates for each of the returns for preturns vector
 rdates <- c() #vector of dates for returns
+diff <- 0
 PArima <- function(DFtest){
   x <- arima(DFtest$GSRatio,c(1,1,0))
   return(c(predict(x,n.ahead = 1)$pred,predict(x,n.ahead = 1)$se))
@@ -22,34 +23,49 @@ PRegression <- function(DFtest){
 
 for(i in btDF$IAUClose){
   
-  if(inTrade == TRUE){     #Calculate unrealized gains/losses
+  if(inTrade == TRUE){     # Calculate unrealized gains/losses
 
     if(gLong == TRUE){
-      preturns <- c(preturns, log((btDF$IAUCLose[end+1])/pGold) + log(pSilv/(btDF$SLVClose[end+1])))
+      preturns <- c(preturns, log((btDF$IAUClose[end+1])/pGold) + log(pSilv/(btDF$SLVClose[end+1])))
       pdates <- c(pdates, index(btDF)[end+1])
-      print("inTrade glong")
-      cat("difference: ",length(preturns)-length(pdates))
-      print(length( (log((btDF$IAUCLose[end+1])/pGold) + log(pSilv/(btDF$SLVClose[end+1])) ) ))
-      print(btDF$IAUCLose[end+1])
-      print(end+1)
-      print("Ay")
+      if(length(pdates)-length(preturns)  > diff){
+        cat("\n")
+        print(index(btDF)[end+1])
+        print("inTrade glong")
+        cat("differnce: ",length(preturns)-length(pdates) )
+        cat("\n")
+        cat("\n")
+        diff <- length(pdates)-length(preturns) 
+      }
     }
     
     if(gLong == FALSE){
       preturns <- c(preturns, log(pGold/(btDF$IAUClose[end+1])) + log((btDF$SLVClose[end+1])/pSilv))
       pdates <- c(pdates, index(btDF)[end+1])
-      print("inTrade gLong is false")
-      cat("differnce: ",length(preturns)-length(pdates) )
-      print("Ay")
+      if(length(pdates)-length(preturns)  > diff){
+        cat("\n")
+        print(index(btDF)[end+1])
+        print("inTrade slong GLONG IS FALSE")
+        cat("differnce: ",length(preturns)-length(pdates) )
+        cat("\n")
+        cat("\n")
+        diff <- length(pdates)-length(preturns) 
+      }
     }
   }
   
   if(inTrade == FALSE){
     preturns <- c(preturns,0)
     pdates <- c(pdates, index(btDF)[end+1])
-    print("inTrade is false")
-    cat("differnce: ",length(preturns)-length(pdates))
-    print("Ay")
+    if(length(pdates)-length(preturns)  > diff){
+      cat("\n")
+      print(index(btDF)[end+1])
+      print("inTrade is FALSE")
+      cat("differnce: ",length(preturns)-length(pdates) )
+      cat("\n")
+      cat("\n")
+      diff <- length(pdates)-length(preturns) 
+    }
   }
   
   prediction <- PArima(btDF[start:end])
@@ -73,10 +89,10 @@ for(i in btDF$IAUClose){
         pSilv <- as.numeric(btDF$SLVClose[end+1]) # price of silv ETF when position was entered
         inTrade <- TRUE
         gLong <- TRUE
-        #cat("Entering Trade at ")
+        cat("Entering Trade at ")
         print(btDF[end+1,0])
-        #cat("Long Gold at ", pGold,"\n")
-        #cat("Short Silver at ",pSilv,"\n")
+        cat("Long Gold at ", pGold,"\n")
+        cat("Short Silver at ",pSilv,"\n")
       }
       
     }
