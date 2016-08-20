55acdcbe6091e78e94c703a5e8c85ede29863b67
Arpan Gupta <arpangupta@Arpans-Air-2.home>
2016-08-20 03:47:50 -0400

    Cleaned up regression code and added residual calculator

--- Regression.R
+++ Regression.R
@@ -1,35 +1,23 @@
 rsquared <- c()
 resids <- c() # residual between actual values and predicted
-start <- 1 # start date for values of rolling regression
-end <- start + 252 # end date for values for rolling regression
+start <- 0 # start date for values of rolling regression
+rollDays <- 252 # number of days to roll
+end <- start + rollDays # end date for values for rolling regression
 actualVal <- c()
 predictedVal <- c()
-money <- 100000
-portdelta <- 0
-portfolio <- c(money)
 
-# USED TO TEST REGRESSION
-# for(i in DFCom$GSRatio){
-#   if(end == length(DFCom$GSRatio)){break}
-#   model <- lm(GSRatio ~ GoldClose + SilvClose + PlatClose, data = DFCom[start:end])
-#   rsquared <- c(rsquared,summary(model1)$adj.r.squared)
-#   predictedVal <- c(predictedVal,predict(model1,DFCom[end+1]))
-#   actualVal <- c(actualVal,DFCom$GSRatio[end+1])
-#   start <- start + 1
-#   end <- start + 252
-#   print(end)
-# }
-start <- 1
-end <- start + 252
-
-for(i in btDF$IAUClose){
-  if(end == length(btDF$IAUClose)){break}
-  model <- lm(GSRatio ~ GoldClose + SilvClose + PlatClose, data = btDF[start:end])
-  predictedVal <- predict(model,btDF[end+1])
-  std <- sd(btDF$GSRatio[start:end])
-    
-  portfolio <- c(portfolio,portfolio + portdelta)
+for(i in index(btDF)){
+  start <- start + 1
+  end <- start + rollDays
+  if(start < rollDays){next}
+  if(end == length(index(btDF))){break}
+  model <- lm(GSRatio ~ GoldClose + SilvClose, data = btDF[start:end])
+  rsquared <- c(rsquared, summary(model)$adj.r.squared)
+  predictedVal <- c(predictedVal, predict(model, btDF[end + 1]))
+  actualVal <- c(actualVal, btDF[end+1]$GSRatio)
 }
 
-predictedVal <- as.xts(predictedVal)
-resids <- as.xts(predictedVal - actualVal)
+resids <- predictedVal - actualVal
+plot(resids, type = "l")
+
+resids <- as.xts(resids)
