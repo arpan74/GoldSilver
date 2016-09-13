0e8cf418455aa426dbbddb470027f59c6d0d4ce9
Arpan Gupta <arpan74@gmail.com>
2016-09-13 14:20:26 -0400

    Added ARIMA Diamond Calculator to optimize speed of optimization program

--- ARIMADF.R
+++ ARIMADF.R
@@ -1,17 +1,26 @@
-DFArima <- c()
+DFArima <- xts(order.by = index(btDF))
+Rpred <- c()
 start <- 0
-rolling_Days <- 10
+rolling_Days <- 20
 end <- start + rolling_Days
+dates <- c()
 
-for(i in index(btDF)){
-  start <- start + 1
-  end <- start + rolling_Days
-  if(start < rollDays){next}
-  if(end == length(index(btDF))){break}
-  
-  model <- arima(btDF$GSRatio[start:end], c(1,1,0))
-  Rpred <- c(Rpred,predict(model, n.ahead = 1)$pred)
-  Rse <- c(Rse, predict(model, n.ahead = 1)$se)
-  
-  dates <- c(dates, i)
+for( rolling_Days in 20:400){
+  for(i in index(btDF)){
+    start <- start + 1
+    end <- start + rolling_Days
+    if(end == length(index(btDF))){break}
+    
+    model <- arima(btDF$GSRatio[start:end], c(1,1,0))
+    Rpred <- c(Rpred, as.numeric(predict(model, n.ahead = 1)$pred))
+    dates <- c(dates, as.Date( index(btDF[end+1]) ) )
+  }
+  test <- xts(Rpred, order.by = as.Date(dates))
+  DFArima <- merge.xts(DFArima, test, fill = NA)
+  print(tail(DFArima))
+  print(rolling_Days)
+  start <- 0
+  Rpred <- c()
+  dates <- c()
 }
+
